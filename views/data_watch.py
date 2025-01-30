3c75e7a2080226922c768414136359b6272320d3
atlatszoiroda <32355085+atlatszoiroda@users.noreply.github.com>
2025-01-30 19:14:00 +0100

    Update data_watch.py

--- views/data_watch.py
+++ views/data_watch.py
@@ -1,20 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from utils_data import read_data
 
 if 'grouped_df' not in st.session_state:
     st.session_state.grouped_df = None    
 
-@st.cache_data(ttl=3600*24)
-def read_data():
-    df = pd.read_parquet('all_eu_money.parquet')
-    df = df.loc[df['megitelt_tamogatas'].notna()]
-    df['megitelt_tamogatas'] = df['megitelt_tamogatas'].astype(int)
-    df = df.sort_values(by='megitelt_tamogatas', ascending=False)
-    df['tam_dont_datum'] = pd.to_datetime(df['tam_dont_datum'], format='%Y.%m.%d').dt.date
-    df['megitelt_tamogatas_eve'] = pd.to_datetime(df['tam_dont_datum'], format='%Y.%m.%d').dt.year
-    df.reset_index(drop=True, inplace=True)
-    return df
+
 df = read_data()
 
 
